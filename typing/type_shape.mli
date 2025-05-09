9d3898ab9200b86450d486c2a75f7c112e9110af
Simon Spies <sspies@janestreet.com>
2025-05-09 14:35:25 +0100

    cleanup

--- typing/type_shape.mli
+++ typing/type_shape.mli
@@ -29,12 +29,34 @@ module Type_shape : sig
 end
 
 module Type_decl_shape : sig
+  type 'a complex_constructor =
+    { name : string;
+      args : 'a complex_constructor_arguments list
+    }
+
+  and 'a complex_constructor_arguments =
+    { field_name : string option;
+      field_value : 'a
+    }
+
+  val complex_constructor_map :
+    ('a -> 'b) -> 'a complex_constructor -> 'b complex_constructor
+
   type tds =
     | Tds_variant of
         { simple_constructors : string list;
-          complex_constructors :
-            (string * (string option * Type_shape.t) list) list
+              (** The string is the name of the constructor. The runtime representation of
+                the constructor at index [i] in this list is [2 * i + 1]. See
+                [dwarf_type.ml] for more details. *)
+          complex_constructors : Type_shape.t complex_constructor list
+              (** All constructors in this category are represented as blocks. The index [i]
+                in the list indicates the tag at runtime. The length of the constructor
+                argument list [args] determines the size of the block. *)
         }
+        (** Note that this variant representation split up variants into immediates
+          (simple constructors) and blocks (complex constructors). Thus, even though the
+          order is disturbed by separating them into two lists, the runtime shape is still
+          uniquely determined, because the two representations are disjoint. *)
     | Tds_record of (string * Type_shape.t) list
     | Tds_alias of Type_shape.t
     | Tds_other
@@ -74,6 +96,5 @@ val type_name :
 
 val attach_compilation_unit_to_paths :
   Type_decl_shape.t -> compilation_unit:Compilation_unit.t -> Type_decl_shape.t
-
 
 val compilation_unit_from_path : Path.t -> string option
--- typing/typedecl.ml
+++ typing/typedecl.ml
@@ -470,29 +470,40 @@ module Shape_reduce = Shape_reduce.Make(struct
 
   end)
 
-let debug = false
+let debug_uid_lookup = false
+(* CR sspies: It is not clear to me whether we want to keep this debugging machinery at
+   all. In its current form, it should at least not slow down the compiler due to dead
+   code elimination. If we determine it is useful, we should perhaps guard it behind a
+   compiler flag. *)
 
 let uid_of_path ~env path =
-  if debug then Format.eprintf "uid_of_path path=%a\n" Path.print path;
+  if debug_uid_lookup then Format.eprintf "uid_of_path path=%a\n" Path.print path;
   let compilation_unit = Type_shape.compilation_unit_from_path path in
   match compilation_unit with
   | None ->
-    if debug then Format.eprintf "I'm in this root\n";
+    if debug_uid_lookup then Format.eprintf "Compilation unit not found\n";
     (match (Env.find_type path env) with
-      | exception Not_found -> if debug then Format.eprintf "None0\n"; None
+      | exception Not_found ->
+        if debug_uid_lookup then Format.eprintf "Type not found -> None\n"; None
       | type_ -> let uid = type_.type_uid in
-        if debug then Format.eprintf "uid = %a\n" Uid.print uid;
+        if debug_uid_lookup then Format.eprintf "Type found -> uid = %a\n" Uid.print uid;
         Some uid
     )
   | Some compilation_unit ->
     let filename = String.uncapitalize_ascii compilation_unit in
     match Load_path.find_normalized (filename ^ ".cms") with
-    | exception Not_found -> if debug then Format.eprintf "None1\n"; None
+    | exception Not_found ->
+      if debug_uid_lookup then Format.eprintf ".cms file not found -> None\n"; None
     | fn ->
       (* CR tnowak: exception? *)
       let cms_infos = Cms_format.read fn in
       match cms_infos.cms_impl_shape with
-      | None -> if debug then Format.eprintf "None2\n"; None
+      | None ->
+        if debug_uid_lookup
+        then Format.eprintf ".cms does not contain implementation shape -> None\n";
+        (* CR sspies: The comment in [cms_format.ml] suggests that this case can not
+           happen, when we load a [.cms] file. Consider throwing an exception instead. *)
+        None
       | Some shape ->
         let shape =
           (* CR tnowak: that might be wrong? *)
@@ -503,8 +514,12 @@ let uid_of_path ~env path =
         in
         let shape = Shape_reduce.reduce env shape in
         match shape.uid with
-        | None -> if debug then Format.eprintf "None3\n"; None
-        | Some uid -> if debug then Format.eprintf "got it uid=%a\n" Uid.print uid; Some uid
+        | None ->
+          if debug_uid_lookup then Format.eprintf "Shape reduction failed -> None\n";
+          None
+        | Some uid ->
+          if debug_uid_lookup then Format.eprintf "Uid from .cms -> uid=%a\n" Uid.print uid;
+          Some uid
 
 
 
